e0a5f8ee5ac74000bfd8e6922ad39ad51375b68f
ChrisPark (durx4) <namegpark@naver.com>
2022-10-31 16:09:12 +0900

    20221031 backend commit
    20221031 backend commit

--- SpyderWeb/app/views.py
+++ SpyderWeb/app/views.py
@@ -10,6 +10,11 @@ import os
 from django.core.files.storage import FileSystemStorage
 from django.http import FileResponse
 
+import numpy as np
+import cv2
+import imutils
+import datetime
+
 @login_required(login_url="/login/")
 def index(request):
     return render(request, "index.html")
@@ -39,10 +44,32 @@ def uploadFile(request):
         fileTitle = request.POST["fileTitle"]
         uploadedFile = request.FILES["uploadedFile"]
 
+        camera = cv2.imread(uploadedFile) 
+        gun_cascade = cv2.CascadeClassifier('cascade.xml')
+        
+        firstFrame = None
+        gun_exist = False
+
+        while True:
+            
+            ret, frame = camera.read()
+        
+            frame = imutils.resize(frame, width = 500)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            gun = gun_cascade.detectMultiScale(gray,
+                                            1.3, 5,
+                                            minSize = (100, 100))
+            
+            if len(gun) > 0:
+                gun_exist = True
+                break
+        
         # Saving the information in the database
         document = models.Document(
             title=fileTitle,
-            uploadedFile=uploadedFile
+            uploadedFile=uploadedFile,
+            gun_exist=gun_exist
         )
         document.save()
 
